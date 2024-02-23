d2ef2ba41927b52e63e312ef72fa13ae03860bb2
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-23 12:14:56 +0100

    add jsnack1

---
+++ jsnack1/main.js
@@ -0,0 +1,15 @@
+// console.log('HI');
+
+const firstNumber = prompt('first numnber')
+
+const secondNumber = prompt('second number')
+
+console.log(firstNumber, secondNumber);
+
+let biggerNumber = secondNumber
+
+if (firstNumber > secondNumber) {
+    biggerNumber = firstNumber
+}
+
+console.log(biggerNumber);
