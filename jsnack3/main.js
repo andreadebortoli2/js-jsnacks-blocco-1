9dbdcf81a14c85f0a1258edcb5bca583c108cee9
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-26 11:03:21 +0100

    add jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,31 @@
+// console.log('HI');
+
+
+let sum = 0
+
+for (let i = 0; i < 10; i++) {
+    
+    const numbers = Number(prompt('writw a number'));
+
+    sum += numbers 
+
+    console.log(sum);
+}
+
+/*
+let i = 0
+
+let sum = 0
+
+while (i < 10) {
+    const numbers = Number(prompt('writw a number'));
+
+    console.log(numbers);
+
+    sum += numbers 
+
+    console.log(sum);
+
+    i++
+};
+*/
